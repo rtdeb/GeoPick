62de698d076674ee2350bc4904682c5f72b8bc7e
arnaldmarcer <arnald.marcer@uab.cat>
2022-12-23 15:56:41 +0000

    mbc and buffer tests

--- R/test4.R
+++ R/test4.R
@@ -106,3 +106,45 @@ plot(buffer_terra, border = "red", add = TRUE, lwd = 3.5)
 
 plot(site)
 plot(st_centroid(site), add=T)
+
+
+par(mfrow=c(1,2))
+rc <- crop(r, ext(-65, -30, 55, 80))
+sf_use_s2(T)
+p1 <- st_as_sf(st_sfc(st_point(c(-50,70)))) %>% st_set_crs(4326)
+b1 <- st_buffer(p1, dist=units::set_units(205, km))
+plot(rc)
+plot(p1, add=T)
+plot(b1, add=T)
+
+b1.co <- data.frame(st_coordinates(b1))
+cat("# BUFFER =========================================\n")
+cat("xmin=",min(b1.co$X), ", xmax=", max(b1.co$X), 
+    ", ymin=", min(b1.co$Y), ", ymax=", max(b1.co$Y), "\n", sep = "")
+cat("dif_x=", max(b1.co$X) - min(b1.co$X), ", dif_y=", max(b1.co$Y) - min(b1.co$Y), sep="")
+
+  
+  
+cat("# MBC =========================================\n")  
+df <- data.frame(lon = c(max(b1.co$X), min(b1.co$X)), lat = c(70, 70))
+point_1 <- df %>%
+  st_as_sf(coords = c("lon", "lat"), crs = 4326) %>%
+  summarise(geometry = st_combine(geometry)) %>%
+  st_cast("MULTIPOINT")
+mbc <- st_minimum_bounding_circle(point_1, nQuadSegs = 30)
+plot(rc)
+plot(mbc, add=T)
+plot(point_1, add=T)
+
+mbc.co <- data.frame(st_coordinates(b1))
+cat("xmin=",min(mbc.co$X), ", xmax=", max(mbc.co$X), 
+    ", ymin=", min(mbc.co$Y), ", ymax=", max(mbc.co$Y), "\n", sep = "")
+cat("dif_x=", max(mbc.co$X) - min(mbc.co$X), ", dif_y=", max(mbc.co$Y) - min(mbc.co$Y), sep="")
+
+
+
+
+
+
+  
+  
---
+++ R/test7.R
@@ -0,0 +1,36 @@
+# MBC
+r <- rast("tmp/wc2.1_10m_elev.tif")
+x <- -50
+y <- 70
+p.4326 <- st_as_sf(st_sfc(st_point(c(x,y)))) %>% st_set_crs(4326)
+buf <- 300000
+# Create buffer
+buffer.4326 <- st_as_sf(terra::buffer(vect(p.4326), buf))
+st_write(buffer.4326, "tmp/buffer-4326.shp", append=F)
+
+# Two opposed points just to check coincidence between buffer and mbc
+xmin <- st_bbox(buffer.4326)["xmin"]
+xmax <- st_bbox(buffer.4326)["xmax"]
+p1 <- st_point(c(xmin, y))
+p2 <- st_point(c(xmax, y))
+pts <- st_multipoint(c(p1, p2))
+pts.4326 <- st_as_sf(st_sfc(pts)) %>% st_set_crs(4326)               
+pts.3857 <- st_transform(pts.4326, 3857)
+
+# Calculate minimum bounding circle with two diameter extremes of buffer
+# Calculate in 3857 to get it correctly, 4326 does use lat/long as equirectangular
+mbc <- st_minimum_bounding_circle(pts.3857, nQuadSegs = 30)
+mbc.4326 <- st_transform(mbc, 4326)
+st_write(mbc.4326, "tmp/mbc-4326.shp", append=F)
+
+plot(rc, main="MBC")
+plot(mbc.4326, add=T)
+plot(p.4326, add=T)
+plot(pts.4326, add=T)
+
+# BUFFER
+plot(rc, main="BUFFER")
+plot(buffer.4326, add=T)
+plot(p.4326, add=T)
+plot(pts.4326, add=T)
+
