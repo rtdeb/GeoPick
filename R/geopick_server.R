f35f67d3273e4569db6303c5bdd652d8ad57f0c5
Arnald Marcer <arnald.marcer@uab.cat>
2023-07-31 10:44:34 +0200

    as.character needed in geopick_server.R

--- R/geopick_server.R
+++ R/geopick_server.R
@@ -6,7 +6,7 @@ library(dplyr)
 library(plumber)
 
 df.env <- read.table("../.env", sep = "=") %>% setNames(., c("var", "value"))
-port <- as.integer(as.character((df.env[df.env$var == "PORT", "value"]))
+port <- as.integer(as.character(df.env[df.env$var == "PORT", "value"]))
 host <- as.character(df.env[df.env$var == "HOST", "value"])
 
 pr("geopick_api.R") %>% pr_run(port = port, host = host)
