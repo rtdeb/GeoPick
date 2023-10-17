f6545d13e0e732f9f12611b5e9516ba9e9e12b47
Arnald Marcer <arnald.marcer@uab.cat>
2023-10-17 17:20:47 +0200

    geopick notebook

--- R/notebook.R
+++ R/notebook.R
@@ -1,4 +1,4 @@
 rmarkdown::render(input = "R/geopick_centroid_uncertainty_calculation.Rmd",
                   output_format = "html_notebook",
                   output_file = "centroid_uncertainty_calculation_nb.html",
-                  output_dir = "../notebooks") 
+                  output_dir = "notebooks/") 
