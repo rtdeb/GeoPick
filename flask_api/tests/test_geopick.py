a721b65538654e20493cfd1d326f960e241f5329
aescobarr <agustiescobar@gmail.com>
2023-11-17 12:42:56 +0100

    Added test

--- flask_api/tests/test_geopick.py
+++ flask_api/tests/test_geopick.py
@@ -16,6 +16,21 @@ def test_env_file_present():
 
 
 def test_api_version():
+    package_path = join(env_dir, 'package.json')
+    f = open(package_path)
+    package_json = json.load(f)
+    v = package_json['version']
+    with t.test_client() as c:
+        rv = c.post('/auth', json={
+            'username': os.environ.get('USERNAME'), 'password': os.environ.get('PASSWORD')
+        })
+        json_response = json.loads(rv.data)
+        token = json_response['token']
+        rversion = c.get('/version', headers={'Authorization' : 'Bearer ' + token})
+        json_response = json.loads(rversion.data)
+        assert json_response['version'] == v
+
+def test_api_auth():
     with t.test_client() as c:
         rv = c.post('/auth', json={
             'username': os.environ.get('USERNAME'), 'password': os.environ.get('PASSWORD')
