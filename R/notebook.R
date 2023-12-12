94d89afad651767dffe0c643ae588cb870d8e09c
Arnald Marcer <arnald.marcer@uab.cat>
2023-12-12 11:07:57 +0100

    deleted old R folder

--- R/notebook.R
+++
@@ -1,4 +0,0 @@
-rmarkdown::render(input = "R/geopick_centroid_uncertainty_calculation.Rmd",
-                  output_format = "html_notebook",
-                  output_file = "centroid_uncertainty_calculation_nb.html",
-                  output_dir = "notebooks/") 
